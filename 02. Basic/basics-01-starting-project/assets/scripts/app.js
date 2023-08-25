f5a3bbe9278b6617083421c5d7df4bb7d152e241
YoungHyun Bae <qodudgus0220@naver.com>
2023-08-25 17:32:17 +0900

    계산기 개발 (with JS)

--- 02. Basic/basics-01-starting-project/assets/scripts/app.js
+++ 02. Basic/basics-01-starting-project/assets/scripts/app.js
@@ -1,13 +1,46 @@
 const defaultResult = 0;
-let currentResult = defaultResult
+let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    alert(`result is ${result}`);
+// input 필드에서 input 값 가져옴
+function getUserNumberInput() {
+  return parseInt(userInput.value);
 }
 
-add(1, 2);
-add(5, 5)
+// 계산 로그 생성
+function createAndWriteLog(operator, beforeCalc, inputNumber) {
+  const calcLog = `${beforeCalc} ${operator} ${inputNumber}`;
+  outputResult(currentResult, calcLog); // from vendor.js file
+}
 
-currentResult = currentResult + 10;
-outputResult(currentResult, '');
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult; // 계산 전 초기값
+  currentResult = currentResult + enteredNumber; // 계산 후 값
+  createAndWriteLog('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteLog('-', initialResult, enteredNumber);
+}
+
+function multiple() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteLog('*', initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteLog('/', initialResult, enteredNumber);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiple);
+divideBtn.addEventListener('click', divide);
