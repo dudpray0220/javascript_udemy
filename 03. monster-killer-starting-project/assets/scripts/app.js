2d6e922b0cd4ae2fe11547423a3d01b71b246886
YoungHyun Bae <qodudgus0220@naver.com>
2023-08-27 21:15:53 +0900

    몬스터 슬레이어 공격기능 개발

--- 03. monster-killer-starting-project/assets/scripts/app.js
+++ 03. monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 13;
 let chosenMaxLife = 100;
 let currentPlayerHealth = chosenMaxLife;
 let currentMonsterHealth = chosenMaxLife;
@@ -6,6 +7,16 @@ let currentMonsterHealth = chosenMaxLife;
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+  const monsterDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= monsterDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('you won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('you lost...');
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert('you have a DRAW');
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
