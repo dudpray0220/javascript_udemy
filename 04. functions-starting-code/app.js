d7f584a0dce86dfe400960d4c76c36bc06279b56
YoungHyun Bae <qodudgus0220@naver.com>
2023-08-30 21:03:56 +0900

    가위바위보 로직 개발

--- 04. functions-starting-code/app.js
+++ 04. functions-starting-code/app.js
@@ -3,6 +3,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WIN = 'PLAYER WIN';
+const RESULT_COMPUTER_WIN = 'COMPUTER WIN';
 
 let gameIsRunning = false;
 
@@ -16,6 +19,31 @@ const getPlayerChoice = function () {
   return selection;
 };
 
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.33) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = function (pChoice, cChoice) {
+  if (pChoice === cChoice) {
+    return RESULT_DRAW;
+  } else if (
+    (pChoice === ROCK && cChoice === SCISSORS) ||
+    (pChoice === SCISSORS && cChoice === PAPER) ||
+    (pChoice === PAPER && cChoice === ROCK)
+  ) {
+    return RESULT_PLAYER_WIN;
+  } else {
+    return RESULT_COMPUTER_WIN;
+  }
+};
+
 startGameBtn.addEventListener('click', function () {
   if (gameIsRunning) {
     return;
@@ -23,6 +51,10 @@ startGameBtn.addEventListener('click', function () {
 
   gameIsRunning = true;
   console.log('Game Start!');
-  const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+  console.log('player', playerChoice);
+  console.log('computer', computerChoice);
+  console.log(winner);
 });
