8b5b85c15f712512afb3d374afb337245b999659
YoungHyun Bae <qodudgus0220@naver.com>
2023-09-01 19:04:33 +0900

    영화 등록 웹 페이지 개발

---
+++ 06. dom-demo-project-setup/assets/scripts/app.js
@@ -0,0 +1,92 @@
+const addMovieModal = document.getElementById('add-modal');
+const startAddMovieButton = document.querySelector('header button');
+// const startAddMovieButton = document.querySelector('header').lastElementChild;
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieModal = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieModal = cancelAddMovieModal.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input');
+const entrySection = document.getElementById('entry-text');
+const movies = [];
+
+const updateUi = () => {
+  if (movies.length === 0) {
+    entrySection.style.display = 'block';
+  } else {
+    entrySection.style.display = 'none';
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement('li');
+  newMovieElement.className = 'movie-element';
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+        <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+        <h2>${title}</h2>
+        <p>${rating}/5 stars</p>
+    </div>
+  `;
+
+  const listRoot = document.getElementById('movie-list');
+  listRoot.append(newMovieElement);
+};
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const clearMovieInput = () => {
+  for (const input of userInputs) {
+    input.value = '';
+  }
+};
+
+const cancelAddMovieHandler = () => {
+  toggleMovieModal();
+  clearMovieInput();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imgValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imgValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert('유효한 값을 입력해주세요 (rating 값은 1 ~ 5 사이)');
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imgValue,
+    rating: ratingValue,
+  };
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInput();
+  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  updateUi();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieModal.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieModal.addEventListener('click', addMovieHandler);
